20493c33a724c988187ff86d0eaf9d34c6da551e
scoomDev <soetaert.chris@gmail.com>
2019-12-30 21:29:21 +0100

    update security.yaml ans routes.yaml && add AuthController.php

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AuthController extends AbstractController
+{
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $em->persist($user);
+        $em->flush();
+
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    public function api()
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
+    }
+}
