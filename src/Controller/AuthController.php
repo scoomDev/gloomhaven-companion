c228d799123694a52e247fe6d6ae17b034ba1e80
soetaert <soetaert.chris@gmail.com>
2020-01-20 21:19:35 +0100

    update register logic

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTManager;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AuthController extends AbstractController
 {
@@ -20,31 +23,30 @@ class AuthController extends AbstractController
 		$this->em = $entityManager;
 	}
 
-	/**
-	 * @Route(
-	 *     name="user_register",
-	 *     path="/api/register",
-	 *     methods={"POST"}
-	 * )
-	 *
-	 * @param Request $request
-	 * @param UserPasswordEncoderInterface $encoder
-	 * @return Response
-	 */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    /**
+     * @Route(
+     *     name="user_register",
+     *     path="/api/register",
+     *     methods={"POST"}
+     * )
+     *
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @param SerializerInterface $serializer
+     * @param JWTTokenManagerInterface $JWTManager $JWTManager
+     * @return Response
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder, SerializerInterface $serializer, JWTTokenManagerInterface $JWTManager)
     {
-        $username = $request->get('username');
-        $password = $request->get('password');
+        $content = $request->getContent();
+        $user = $serializer->deserialize($content, User::class, 'json');
 
-        if ($this->em->getRepository(User::class)->findBy(['username' => $username])) return new JsonResponse(['error' => 'Ce nom d\'utilisateur est déjà utilisé']);
+        if ($this->em->getRepository(User::class)->findBy(['username' => $user->getUsername()])) return new JsonResponse(['error' => 'Ce nom d\'utilisateur est déjà utilisé']);
 
-        $user = new User();
-		$user->setUsername($username);
-		$user->setPassword($encoder->encodePassword($user, $password));
-
+        $user->setRoles(['ROLE_USER']);
 		$this->em->persist($user);
 		$this->em->flush();
 
-		return new JsonResponse(['success' => 'Utilisateur bien enregistré']);
+		return new JsonResponse(['success' => 'Utilisateur bien enregistré', 'token' => $JWTManager->create($user)]);
     }
 }
